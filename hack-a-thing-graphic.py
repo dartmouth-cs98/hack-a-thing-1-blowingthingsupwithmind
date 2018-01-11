0299dee2d355a0cbb08d76227a671668dda782e4
brophyt <william.b.tyree.18@dartmouth.edu>
2018-01-11 13:06:59 -0500

    Add files via upload
    Visualization graphic code and library

---
+++ hack-a-thing-graphic.py
@@ -0,0 +1,50 @@
+import graphics
+import time, os, sys
+
+def main():
+    win = graphics.GraphWin("My Circle", 300, 300)
+    win.setBackground("grey")
+    radius = 50
+
+    rPoint1 = graphics.Point(100,100)
+    rPoint2 = graphics.Point(200,200)
+
+    r = graphics.Rectangle(rPoint1, rPoint2)
+
+    aPoint1 = graphics.Point(0,0)
+    aPoint2 = graphics.Point(100,100)
+    bPoint1 = graphics.Point(0,300)
+    bPoint2 = graphics.Point(100,200)
+    cPoint1 = graphics.Point(300,0)
+    cPoint2 = graphics.Point(200,100)
+    dPoint1 = graphics.Point(300,300)
+    dPoint2 = graphics.Point(200,200)
+
+    aLine = graphics.Line(aPoint1, aPoint2)
+    bLine = graphics.Line(bPoint1, bPoint2)
+    cLine = graphics.Line(cPoint1, cPoint2)
+    dLine = graphics.Line(dPoint1, dPoint2)
+
+    aLine.draw(win)
+    bLine.draw(win)
+    cLine.draw(win)
+    dLine.draw(win)
+
+    r.draw(win)
+
+    c = graphics.Circle(graphics.Point(150,150), radius)
+    c.setFill("green")
+    c.draw(win)
+
+    while True :
+        if win.getKey() == 'd' :
+            sys.stdout.write("Dropping back")
+            radius = radius - 5
+            sys.stdout.write("New Radius is " + str(radius))
+            c.undraw()
+            c = graphics.Circle(graphics.Point(150,150), radius)
+            c.setFill("green")
+            c.draw(win)
+    win.close()
+
+main()
