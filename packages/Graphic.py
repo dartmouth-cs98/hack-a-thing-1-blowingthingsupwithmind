28284d5bd374abfbb45af8d8a715c19ac0859e47
brophyt <william.b.tyree.18@dartmouth.edu>
2018-01-11 15:54:52 -0500

    Update Graphic.py
    added array integration

--- packages/Graphic.py
+++ packages/Graphic.py
@@ -1,49 +1,51 @@
 import packages.graphics as graphics
 import time, os, sys
 
-def main():
-    win = graphics.GraphWin("My Circle", 300, 300)
-    win.setBackground("grey")
+def render(array):
+        win = graphics.GraphWin("My Circle", 300, 300)
+        win.setBackground("grey")
 
-    radius = 50
+        radius = 50
 
-    rPoint1 = graphics.Point(100,100)
-    rPoint2 = graphics.Point(200,200)
+        rPoint1 = graphics.Point(100,100)
+        rPoint2 = graphics.Point(200,200)
 
-    r = graphics.Rectangle(rPoint1, rPoint2)
+        r = graphics.Rectangle(rPoint1, rPoint2)
 
-    aPoint1 = graphics.Point(0,0)
-    aPoint2 = graphics.Point(100,100)
-    bPoint1 = graphics.Point(0,300)
-    bPoint2 = graphics.Point(100,200)
-    cPoint1 = graphics.Point(300,0)
-    cPoint2 = graphics.Point(200,100)
-    dPoint1 = graphics.Point(300,300)
-    dPoint2 = graphics.Point(200,200)
+        aPoint1 = graphics.Point(0,0)
+        aPoint2 = graphics.Point(100,100)
+        bPoint1 = graphics.Point(0,300)
+        bPoint2 = graphics.Point(100,200)
+        cPoint1 = graphics.Point(300,0)
+        cPoint2 = graphics.Point(200,100)
+        dPoint1 = graphics.Point(300,300)
+        dPoint2 = graphics.Point(200,200)
 
-    aLine = graphics.Line(aPoint1, aPoint2)
-    bLine = graphics.Line(bPoint1, bPoint2)
-    cLine = graphics.Line(cPoint1, cPoint2)
-    dLine = graphics.Line(dPoint1, dPoint2)
+        aLine = graphics.Line(aPoint1, aPoint2)
+        bLine = graphics.Line(bPoint1, bPoint2)
+        cLine = graphics.Line(cPoint1, cPoint2)
+        dLine = graphics.Line(dPoint1, dPoint2)
 
-    aLine.draw(win)
-    bLine.draw(win)
-    cLine.draw(win)
-    dLine.draw(win)
+        aLine.draw(win)
+        bLine.draw(win)
+        cLine.draw(win)
+        dLine.draw(win)
 
-    r.draw(win)
+        r.draw(win)
 
-    c = graphics.Circle(graphics.Point(150,150), radius)
-    c.setFill("green")
-    c.draw(win)
+        c = graphics.Circle(graphics.Point(150,150), radius)
+        c.setFill("green")
+        c.draw(win)
 
-    while True :
-        if win.getKey() == 'd' :
+        t = 0
+        while t < 60 :
             sys.stdout.write("Dropping back")
-            radius = radius - 5
+            radius = array.pop(0)
             sys.stdout.write("New Radius is " + str(radius))
             c.undraw()
             c = graphics.Circle(graphics.Point(150,150), radius)
             c.setFill("green")
             c.draw(win)
-    win.close()
+            time.sleep(0.25)
+            t = t+1
+        win.close()
